bf50186cfc71f4c035236099b2e8e7ffc0201154
qhduan <mail@qhduan.com>
2022-03-04 16:26:07 +0800

    set default token

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from fastapi.responses import HTMLResponse
 from app import app
 from search_qa import search_qa_format
 from items import Item_jzmh
-from jzmh import send_message
+from jzmh import send_message, default_token
 import api_config
 from open_search import current_dir
 
@@ -33,7 +33,8 @@ async def receive_message(item: Item_jzmh, request: Request, jzmj_token: str= No
             "data": { "payload": { "text": "hello" }, "chatId": "fake" }
         }'
     """
-    print('jzmj_token', jzmj_token)
+    if jzmj_token is None:
+        jzmj_token = default_token
     search_text = item.data.payload.text
     format_return_data = search_qa_format(search_text, token=jzmj_token)
     return send_message(chatid=item.data.chatId, text=format_return_data, token=jzmj_token)
